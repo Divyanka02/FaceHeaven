e83f07abdf7fb241618384e3d83adbfcdfca42bd
Divyanka02 <85795778+Divyanka02@users.noreply.github.com>
2023-03-26 11:22:29 +0530

    Add files via upload

---
+++ feedback.php
@@ -0,0 +1,22 @@
+<?php
+include 'database.php';
+if(isset($_POST['submit']))
+{
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $message=$_POST['message'];
+   
+    $sql="insert into ourfeedback(name,email,message)
+    values('$name','$email','$message')";
+    
+    if(mysqli_query($con,$sql))
+    {
+        echo "<script>alert('new record insert successfully')</script>";
+    }
+    else
+    {
+        echo "error:" .mysqli_error($con);
+    }
+    mysqli_close($con);
+}
+?>
